1931cb8c502dabd6607af17262b62be16277891c
fabniklett <fabian.kolett@gmail.com>
2023-12-05 18:55:45 +0100

    routes given for different components, extra components created, main page first changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {MainPageComponent} from "./main-page/main-page.component";
+import {WorkProjectsComponent} from "./work-projects/work-projects.component";
+import {ContactComponent} from "./contact/contact.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: "mainpage", component: MainPageComponent},
+  {path: "munkaim", component: WorkProjectsComponent},
+  {path: "kapcsolat", component: ContactComponent},
+  {path: '**', redirectTo: 'page-not-found', pathMatch: 'full'},
+];
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-main-page',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("mainpage")
+  }
 
 }
