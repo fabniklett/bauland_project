c06330807c660c4c6ad123657ad9bb45aaf110b8
fabniklett <fabian.kolett@gmail.com>
2023-12-05 16:51:09 +0100

    new components in Angular are created: main page, contact page, work projects page

---
+++ src/app/work-projects/work-projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './work-projects.component.html',
+  styleUrl: './work-projects.component.css'
+})
+export class WorkProjectsComponent {
+
+}
